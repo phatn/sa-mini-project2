c368182596bc6a9c2b132054fdfd19069ad3a2f2
tuan.anh.nguyen <oliver.anh.nguyen@gmail.com>
2022-10-15 15:30:50 -0500

    Init shipment service

---
+++ shipment-service/src/test/java/edu/miu/shipmentservice/ShipmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.shipmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
