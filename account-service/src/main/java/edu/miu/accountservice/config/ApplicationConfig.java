80100633b10e09749bb81d0e6fe9f0bf4947bab0
tuan.anh.nguyen <oliver.anh.nguyen@gmail.com>
2022-10-16 17:42:48 -0500

    clean code

--- account-service/src/main/java/edu/miu/accountservice/config/ApplicationConfig.java
+++ account-service/src/main/java/edu/miu/accountservice/config/ApplicationConfig.java
@@ -5,34 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.client.RestTemplate;
-
-import javax.annotation.PreDestroy;
-import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
 @Slf4j
 public class ApplicationConfig {
-
-    private final JdbcTemplate jdbcTemplate;
-
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
         return modelMapper;
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
-    @PreDestroy
-    public void onExit() {
-        log.info("Cleaning up database....");
-        jdbcTemplate.execute("DELETE FROM account");
-        log.info("Database cleaned!");
     }
 }
