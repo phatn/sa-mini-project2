05374e14d4a7ae582ca98f656fc5e20acea32f67
Phat Nguyen <phatnguyentanit@gmail.com>
2022-10-18 23:26:33 -0500

    Added README.MD

--- shutdown_services.sh
+++ shutdown_services.sh
@@ -10,10 +10,10 @@ kubectl delete -f kubernetes/services/paypal-deployment.yml
 kubectl delete -f kubernetes/services/product-deployment.yml
 kubectl delete -f kubernetes/services/shipping-deployment.yml
 
-#kubectl delete -f kubernetes/dependencies/mysql-pv.yml
-#kubectl delete -f kubernetes/dependencies/mysql-deployment.yml
-#kubectl delete -f kubernetes/dependencies/keycloak-deployment.yml
+kubectl delete -f kubernetes/dependencies/mysql-pv.yml
+kubectl delete -f kubernetes/dependencies/mysql-deployment.yml
+kubectl delete -f kubernetes/dependencies/keycloak-deployment.yml
 
-#kubectl delete -f kubernetes/config/mysql-init-configmap.yml
-#kubectl delete -f kubernetes/config/service-configmap.yml
-#kubectl delete -f kubernetes/config/service-secret.yml
+kubectl delete -f kubernetes/config/mysql-init-configmap.yml
+kubectl delete -f kubernetes/config/service-configmap.yml
+kubectl delete -f kubernetes/config/service-secret.yml
